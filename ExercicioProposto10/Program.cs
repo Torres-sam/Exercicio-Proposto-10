e8a44b2ba340605095a86720ee11613590d50a6b
Samuel Torres <samuelvianatorres@gmail.com>
2025-08-02 21:02:09 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioProposto10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace MyApp
+{
+    internal class ExercicioProposto10
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a hora inicial e final do jogo: ");
+            string[] horas = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(horas[0]);
+            int horaFinal = int.Parse(horas[1]);
+            int duracao;
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            }
+            else
+            {
+                duracao =  24 - horaInicial + horaFinal;
+            }
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
